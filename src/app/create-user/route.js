70309fe57b7ece97d06728130480843763efdd3d
John Pham <johnphammail@gmail.com>
2023-05-19 15:09:53 -0700

    add comments

--- src/app/create-user/route.js
+++ src/app/create-user/route.js
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 export async function POST(request) {
   // This causes the page to hang.
   // We want to redirect back to the home page at /.
+  // This works on next@13.2.4, does not work on next@13.4.3.
   redirect("/");
 
   // This works
